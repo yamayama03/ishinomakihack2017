0d9482fee1a55649de014e2e431edc8e12458e5d
hrk fkm <fkm@mokelab.com>
2017-07-29 12:53:24 +0900

    add kii library

--- js/Application.ts
+++ js/Application.ts
@@ -1,7 +1,12 @@
+/// <reference path="./kii.d.ts"/>
+var APP_ID = 'orueuntaxbsi';
+var APP_KEY = 'c681148710d045fe9ad1bc94f4a209b0';
+
 class Application {
     router : any;
     page : Page;
     start() {
+        Kii.initializeWithSite(APP_ID, APP_KEY, KiiSite.JP);
     }
 
     showPage(page : string) {
