27fcfe9a54fb64b842be2e2396f1fb1e648b9630
yoshidakai <exiaoor.wgzc@gmail.com>
2017-07-29 21:25:41 +0900

    sendCommentの作成

--- js/app.js
+++ js/app.js
@@ -155,8 +155,15 @@ var ArticlePage = (function () {
         this.ractive = new Ractive({
             el: '#container',
             template: '#ArticleTemplate',
-            showNext: function () {
-                _this.app.showPage('second/1234');
+            sendComment: function () {
+                var comment = _this.ractive.get("comment");
+                var obj = Kii.bucketWithName("comment").createObject();
+                obj.set("parent", _this.id);
+                obj.set("comment", comment);
+                obj.save().then(function (o) {
+                    alert("投稿しました");
+                    window.history.back();
+                });
             }
         });
         var obj = KiiObject.objectWithURI("KiiCloud://buckets/anger/objects/" + this.id);
@@ -165,6 +172,13 @@ var ArticlePage = (function () {
             var text = o.get("text");
             var point = o.get("point");
             _this.ractive.set({ title: title, text: text, point: point });
+        });
+        obj = KiiObject.objectWithURI("KiiCloud://buckets/comment/objects/");
+        var bucket = Kii.bucketWithName("comment");
+        var allQuery = KiiQuery.queryWithClause(KiiClause.equals("parent", this.id));
+        allQuery.sortByDesc("_created");
+        bucket.executeQuery(allQuery).then(function (v) {
+            _this.ractive.set("list", v[1]);
         });
     };
     return ArticlePage;
