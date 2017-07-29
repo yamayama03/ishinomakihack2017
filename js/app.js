e3f241e30bbbacbf149757f54f488125e3877793
yoshidakai <exiaoor.wgzc@gmail.com>
2017-07-29 16:15:22 +0900

    Userにポイントを追加

--- js/app.js
+++ js/app.js
@@ -76,6 +76,10 @@ var NewUserPage = (function () {
                 var password = _this.ractive.get("password");
                 var user = KiiUser.userWithEmailAddress(email, password);
                 user.register().then(function (theUser) {
+                    var obj = KiiObject.objectWithURI("kiicloud://buckets/user/objects/" + theUser.getID());
+                    obj.set("point", 0);
+                    return obj.saveAllFields();
+                }).then(function (o) {
                     alert("成功");
                 })["catch"](function (error) {
                     var theUser = error.target;
